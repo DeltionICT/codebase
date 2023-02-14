ce536f2ae953ed05261d021932937f484d6f92bb
Jan Jaap Siewers <siewers32@gmail.com>
2023-02-14 21:20:39 +0100

    volgorde items aangepast

--- .eleventy.js
+++ .eleventy.js
@@ -70,8 +70,12 @@ module.exports = function(eleventyConfig) {
             // (p.data.key) ? console.log(p.data.key):console.log("bla");
             return (p.data.key) ? keys.includes(p.data.key) : false;
         })
-        console.log(x)
-        return x
+        items = []
+        keys.forEach(key =>
+            items.push(x.find(item => item.data.key == key))
+        );
+        console.log(items)
+        return items
 	});
 
     eleventyConfig.addFilter('toArray', (text) => {
